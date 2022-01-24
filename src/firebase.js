64aec9c8badddd7c5562c7bd6ad92beec58d947c
anoushka <anouska2018cs141abesit@gmail.com>
2022-01-25 00:16:11 +0530

    minor changes in firebse  variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import firebase from "firebase";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const firebaseConfig = {
-  apiKey: "YOUR API KEY",
-  authDomain: "YOUR DOMAIN",
-  projectId: "YOUR PROJECT ID",
-  storageBucket: "YOUR STORAGE BUCKET",
-  messagingSenderId: "YOUR MESSAGING SENDER ID",
-  appId: "YOUR APP ID",
-  measurementId: "YOUR MEASUREMENT ID",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+// measurementId: "YOUR MEASUREMENT ID",
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
